3ebf9ccd6c997561be80302098eff4b2cc03b61e
Sarah Sanders <72199180+shiressl1@users.noreply.github.com>
2022-12-08 10:48:20 +0000

    Update test_models.py
    Corrected the assert

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,4 +4,4 @@ def test_new_user_not_logged_in(general_public_user):
     WHEN the user has just been created
     THEN the logged in status should be False
     """
-    assert general_public_user.is_logged_in is True
+    assert general_public_user.is_logged_in is False
